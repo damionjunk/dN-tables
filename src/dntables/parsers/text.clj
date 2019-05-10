9d1393bf2a0433ff826dad0a07d5d04a3b23bb20
Damion Junk <junkda@purdue.edu>
2019-05-09 21:08:49 -0500

    adding simple text parser that emits EDN

---
+++ src/dntables/parsers/text.clj
@@ -0,0 +1,95 @@
+(ns dntables.parsers
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(defn source? [text]
+  (when text (some? (re-find #"^::source" (s/trim text)))))
+
+(defn parse-source [text]
+  (second (re-find #"^::source\s+(.*)" text)))
+
+(defn author? [text]
+  (when text (some? (re-find #"^::author" (s/trim text)))))
+
+(defn parse-author [text]
+  (second (re-find #"^::author\s+(.*)" text)))
+
+(defn title? [text]
+  (when text (some? (re-find #"^[\[\()]|^[A-Za-z!-.:$#@]" (s/trim text)))))
+
+(defn parse-title [text]
+  (or (second (re-find #"^[\[\(]\s?d\s?\d+\s?[\]\)]\s?(.*)" text))
+      (second (re-find #"^(.*)[\[\(]\s?d\s?\d+\s?[\]\)]\s?$" text))
+      ))
+
+(defn entry? [text]
+  (when text (some? (re-find #"^\d" (s/trim text)))))
+
+(defn parse-entry [text]
+  (second (re-find #"^\d+[\s-:.=\>]+(.*)" text)))
+
+(defn ->empty->nil [x] (if (nil? x) nil (if (empty? (s/trim x)) nil (s/trim x))))
+
+(defn simple-reader
+  "Has the following assumptions about a text file:
+ 
+   The first text encountered will be the title of the random table. 
+   The title may start with, or end with `\\[d\\d+\\]` to indicate the dice type, 
+   even though it is implied by the number of entires, so this is not necessary.
+
+   Each following line represents an entry in the random table.
+   Each entry must be prefixed with `\\d+\\s+[.-:\\s]`
+   Entries continue until EOL or a new title is encountered."
+  [file]
+  (with-open [rdr (io/reader file)]
+    (let [everything
+          (reduce
+           (fn [{collecting? :collecting? items :items title :title parsed :parsed :as m} line]
+             (if-let [line (->empty->nil line)]
+               (cond
+                 (source? line) (assoc m :source (->empty->nil (parse-source line)))
+                 (author? line) (assoc m :author (->empty->nil (parse-author line)))
+
+                 ;; Add the item to the items list.
+                 (and collecting? (entry? line)) (if-let [item (->empty->nil (parse-entry line))]
+                                                   (assoc m :items (conj (or items []) item))
+                                                   m)
+
+                 ;; Add the title, prep for collecting.
+                 (and (not collecting?) (title? line)) (if-let [title (->empty->nil (parse-title line))]
+                                                         (assoc m :title title :collecting? true)
+                                                         m)
+
+                 ;; Store previous and prep for next round.
+                 (and collecting? (title? line)) (if-let [title (->empty->nil (parse-title line))]
+                                                   (assoc m
+                                                          :parsed (conj (or parsed [])
+                                                                        {:title (:title m)
+                                                                         :author (:author m)
+                                                                         :source (:source m)
+                                                                         :count (count (:items m))
+                                                                         :items (:items m)})
+                                                          :items []
+                                                          :title title)
+                                                   m)
+                 :default m)
+               m))
+           {}
+           (line-seq rdr))]
+      (if (pos? (count (:items everything)))
+        (conj (or (:parsed everything) [])
+              {:title (:title everything)
+               :author (:author everything)
+               :source (:source everything)
+               :count (count (:items everything))
+               :items (:items everything)})
+        (:parsed everything)))))
+
+
+(comment
+
+  (parse-title "this is a title [ d 12 ]")
+
+  (clojure.pprint/pprint
+   (simple-reader (io/resource "goatmansgoblet/familyweapons.txt")))  
+  )
